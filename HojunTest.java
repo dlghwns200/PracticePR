66e4e7b30240361d66bbc6ed5c7a522147143bcd
dlghwns200 <dlghwns200@gmail.com>
2025-04-07 15:25:33 +0900

    테스트2

--- HojunTest.java
+++ HojunTest.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class HojunTest {
+    public static void main(String[] args) {
+        System.out.println("PR 테스트");
+        System.out.println("眠い");
+        System.out.println("Test");
+
+    }
 }
